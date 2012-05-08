b55acc34f0df54a48665eb3fbc7d9eb024dd7cb2
Chmouel Boudjnah <chmouel@chmouel.com>
2012-05-08 11:17:04 +0100

    First commit.

---
+++ README.rst
@@ -0,0 +1,20 @@
+Python bindings to the OpenStack Object Storage API
+===================================================
+
+This is a python client for the Swift API. There's a Python API (the
+``swiftclient`` module), and a command-line script (``swift``).
+
+Development takes place via the usual OpenStack processes as outlined
+in the `OpenStack wiki`_.  The master repository is on GitHub__.
+
+__ http://wiki.openstack.org/HowToContribute
+__ http://github.com/openstack/python-swiftclient
+
+This code is based on original the client previously included with
+`OpenStack's swift`__ The python-swiftclient is licensed under the
+Apache License like the rest of OpenStack.
+
+__ http://github.com/openstack/swift
+
+.. contents:: Contents:
+   :local:
---
+++ setup.py
@@ -0,0 +1,44 @@
+# -*- encoding: utf-8 -*-
+import os
+import setuptools
+import sys
+
+# TODO: Figuring out how we are going to do the versionning (and if
+# any).
+version = '0.1'
+name = 'python-swiftclient'
+requires = []
+
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+if sys.version_info < (2, 6):
+    requires.append('simplejson')
+
+setuptools.setup(
+    name=name,
+    version=version,
+    description='Client Library for OpenStack Object Storage API',
+    long_description=read('README.rst'),
+    url='https://github.com/chmouel/python-swiftclient',
+    license='Apache License (2.0)',
+    author='OpenStack, LLC.',
+    author_email='openstack-admins@lists.launchpad.net',
+    packages=setuptools.find_packages(exclude=['tests', 'tests.*']),
+    install_requires=requires,
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Information Technology',
+        'License :: OSI Approved :: Apache Software License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2.6',
+        'Environment :: No Input/Output (Daemon)',
+    ],
+    test_suite='nose.collector',
+    scripts=[
+        'bin/swift',
+    ],
+)
