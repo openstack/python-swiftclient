b5ed14f90a8214700a9be2139d9e7aba82baaa99
Doug Hellmann <doug@doughellmann.com>
2018-03-22 18:04:06 -0400

    add lower-constraints job
    Create a tox environment for running the unit tests against the lower
    bounds of the dependencies.
    
    Create a lower-constraints.txt to be used to enforce the lower bounds
    in those tests.
    
    Add openstack-tox-lower-constraints job to the zuul configuration.
    
    Update the dependencies needed to make the unit tests pass while
    constrained to the lower bounds.
    
    See http://lists.openstack.org/pipermail/openstack-dev/2018-March/128352.html
    for more details.
    
    Co-Authored-By: Nguyen Hai <nguyentrihai93@gmail.com>
    Change-Id: I2a8f465c8b08370517cbec857933b08fca94ca38
    Depends-On: https://review.openstack.org/555034
    Signed-off-by: Doug Hellmann <doug@doughellmann.com>

--- setup.py
+++ setup.py
@@ -17,10 +17,16 @@
 # THIS FILE IS MANAGED BY THE GLOBAL REQUIREMENTS REPO - DO NOT EDIT
 import setuptools, sys
 
-if sys.version_info < (2, 7):
-    sys.exit('Sorry, Python < 2.7 is not supported for'
-             ' python-swiftclient>=3.0')
+import setuptools
+
+# In python < 2.7.4, a lazy loading of package `pbr` will break
+# setuptools if some other modules registered functions in `atexit`.
+# solution from: http://bugs.python.org/issue15881#msg170215
+try:
+    import multiprocessing  # noqa
+except ImportError:
+    pass
 
 setuptools.setup(
-    setup_requires=['pbr'],
+    setup_requires=['pbr>=2.0.0'],
     pbr=True)
