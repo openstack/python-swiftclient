afa2c4642a467862a34dea48dc2bd4c9203c6a2a
jinyuanliu <liujinyuan@inspur.com>
2021-09-15 01:54:42 -0400

    Clean up extra spaces
    Although these errors are not important, they affect the code specification.
    
    Change-Id: Ifab29d9c803a78ee994b99b4b410893864b90908

--- run_tests.sh
+++ run_tests.sh
@@ -18,7 +18,7 @@ command -v tox > /dev/null 2>&1
 if [ $? -ne 0 ]; then
   echo 'This script requires "tox" to run.'
   echo 'You can install it with "pip install tox".'
-  exit 1; 
+  exit 1;
 fi
 
 just_pep8=0
